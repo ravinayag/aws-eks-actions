e71fa32e98e9e77459bd4a02d0bde1295ffdd742
Ravi <ravinayag@gmail.com>
2021-11-28 12:24:27 +0530

    corrections

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,6 +4,6 @@ set -e
 
 # Extract the base64 encoded config data and write this to the KUBECONFIG
 echo "$KUBECONFIG_SECRET" | base64 -d > /tmp/config
-export KUBECONFIG=/tmp/config
+export KUBECONFIG_SECRET=/tmp/config
 
 sh -c "kubectl $*"
