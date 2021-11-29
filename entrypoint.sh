e661cff2e2418ae5f5576d871d889ced32c58dbd
Ravi <ravinayag@gmail.com>
2021-11-29 20:08:14 +0530

    corrections

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,10 +2,10 @@
 
 set -e
 
-sh -c "aws configure set region ${aws_region}"
-sh -c "aws configure set aws_access_key_id ${aws_access_key_id}"
-sh -c "aws configure set aws_secret_access_key ${aws_secret_access_key}"
-sh -c "aws eks update-kubeconfig --region ${aws_region} --name ${cluster_name}"
+sh -c "aws configure set region ${aws-region}"
+sh -c "aws configure set aws_access_key_id ${aws-access-key-id}"
+sh -c "aws configure set aws_secret_access_key ${aws-secret-access-key}"
+sh -c "aws eks update-kubeconfig --region ${aws-region} --name ${cluster-name}"
 
 #Debug
 sh -c "ls -alrt"
