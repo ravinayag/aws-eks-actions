8263ae99083403fc0a9d8e3da5c41452810f94c3
Ravi <ravinayag@gmail.com>
2021-11-29 23:17:01 +0530

    corrections

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,10 +9,10 @@ echo ${AWS_REGION}:${AWS_ACCESS_KEY_ID}:${AWS_SECRET_ACCESS_KEY}:${CLUSTER_NAME}
 # sh -c "aws configure set aws_secret_access_key ${aws_secret_access_key}"
 # sh -c "aws eks update-kubeconfig --region ${aws_region} --name ${cluster_name}"
 
-# sh -c "aws configure set region ${AWS_REGION}"
-# sh -c "aws configure set aws_access_key_id ${AWS_ACCESS_KEY_ID}"
-# sh -c "aws configure set aws_secret_access_key ${AWS_SECRET_ACCESS_KEY}"
-# sh -c "aws eks update-kubeconfig --region ${AWS_REGION} --name ${CLUSTER_NAME}"
+sh -c "aws configure set region ${AWS_REGION}"
+sh -c "aws configure set aws_access_key_id ${AWS_ACCESS_KEY_ID}"
+sh -c "aws configure set aws_secret_access_key ${AWS_SECRET_ACCESS_KEY}"
+sh -c "aws eks update-kubeconfig --region ${AWS_REGION} --name ${CLUSTER_NAME}"
 
 
 # Extract the base64 encoded config data and write this to the KUBECONFIG
