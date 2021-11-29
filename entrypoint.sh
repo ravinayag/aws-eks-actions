0a8a1e060552bed130b32b2fbdfdaff453296448
Ravi <ravinayag@gmail.com>
2021-11-29 23:27:22 +0530

    corrections

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,13 +1,6 @@
 #!/bin/sh
 
-set -xe
-echo ${aws-region}:${aws-access-key-id}:${aws-secret-access-key}:${cluster-name}
-
-echo ${AWS_REGION}:${AWS_ACCESS_KEY_ID}:${AWS_SECRET_ACCESS_KEY}:${CLUSTER_NAME}
-# sh -c "aws configure set region ${aws_region}"
-# sh -c "aws configure set aws_access_key_id ${aws_access_key_id}"
-# sh -c "aws configure set aws_secret_access_key ${aws_secret_access_key}"
-# sh -c "aws eks update-kubeconfig --region ${aws_region} --name ${cluster_name}"
+set -e
 
 sh -c "aws configure set region ${AWS_REGION}"
 sh -c "aws configure set aws_access_key_id ${AWS_ACCESS_KEY_ID}"
@@ -20,4 +13,3 @@ echo "$KUBECONFIG_SECRET" | base64 -d > /tmp/config
 export KUBECONFIG=/tmp/config
 
 sh -c "kubectl $*"
-#sh -c "$*"
